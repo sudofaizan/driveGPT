aa11c4cb20ffbbba54e567b50423cdacd2ace975
Faizan quazi <faizan_quazi@intuit.com>
2024-09-23 21:37:18 +0530

    Stable

--- sourcecode/main/main.ino
+++ sourcecode/main/main.ino
@@ -1,18 +1,16 @@
-// |———————————————————————————————————————————————————————| 
-// |  made by Arduino_uno_guy 11/13/2019                   |
-// |   https://create.arduino.cc/projecthub/arduino_uno_guy|
-//  |———————————————————————————————————————————————————————|
-
-
 #include <LiquidCrystal_I2C.h>
 #include <SoftwareSerial.h>
 #include <Wire.h>
-SoftwareSerial btm(11, 10); // RX, TX
+#include "ELMduino.h"
 
-//initialize the liquid crystal library
-//the first parameter is  the I2C address
-//the second parameter is how many rows are on your screen
-//the  third parameter is how many columns are on your screen
+SoftwareSerial btm(11, 10); // RX, TX init onboard btm
+#define ELM_PORT btm
+uint32_t rpm = 0;
+
+uint32_t load = 0;
+bool connected = false;
+ELM327 myELM327;
+
 LiquidCrystal_I2C lcd(0x27,  16, 2);
 const int LED_YELLOW = 12;
 const int LED_BLUE = 13;
@@ -21,16 +19,64 @@ const int BTM_KEY = 9;
 const int BTM_RX = 11;
 const int BTM_TX = 10;
 const int BUZZ = 7;
+void checkcar(){
+    btm.println("ATZ"); // Send "AT" command
+  delay(300);
+  String response = btm.readStringUntil('\n');
+response.trim();
+  Serial.println(response);
 
+if (response.indexOf("ELM327 v1.5") != -1) {
+  Serial.println("car detected");
+      digitalWrite(LED_YELLOW, 1);
+        lcd.clear();
+        lcd.setCursor(0,0);
+        lcd.print("CAR Connected");
+        connected = true;  // Set connected to true after first run
+        digitalWrite(BUZZ, 1);
+        delay(50);
+        digitalWrite(BUZZ, 0);
+        delay(50);
+        digitalWrite(BUZZ, 1);
+        delay(50);
+        digitalWrite(BUZZ, 0);
+        delay(50);
+        digitalWrite(BUZZ, 1);
+        delay(50);
+        digitalWrite(BUZZ, 0);
+        delay(50);
+        digitalWrite(BUZZ, 1);
+        delay(50);
+        digitalWrite(BUZZ, 0);
+        digitalWrite(LED_YELLOW, 1);
+  if (!myELM327.begin(ELM_PORT, true, 2000))
+  {
+    Serial.println("Couldn't connect to OBD scanner");
+    while (1);
+  }
+
+
+
+
+} else {
+  digitalWrite(BUZZ, 1);
+  Serial.println("car not found");
+        lcd.clear();
+      lcd.setCursor(0,0);
+    lcd.print("CAR Not FOund");
+  digitalWrite(BUZZ, 0);
+    checkcar();
+
+
+}
+}
 void setup() {
   Serial.begin(9600);
-  Serial.println("Enter AT commands:");
   btm.begin(38400);
   pinMode(LED_BLUE, OUTPUT); //Blue led 
   pinMode(LED_YELLOW, OUTPUT); //Yello led
   pinMode(BTM, OUTPUT);  //BTM
-  pinMode(BTM_KEY, OUTPUT);  //BTM
-  pinMode(BTM, OUTPUT);  //BTM
+  pinMode(BTM_KEY, OUTPUT);  //BTM key
   pinMode(BUZZ, OUTPUT); //buzzer
   //initialize lcd screen
   lcd.init();
@@ -49,29 +95,67 @@ void setup() {
     // Serial.println(response);
 
 if (response.endsWith("OK")) {
-  Serial.println("Connected");
+  Serial.println("got ok back");
+  digitalWrite(BTM, 1);
   digitalWrite(LED_YELLOW, 1);
   digitalWrite(BUZZ, 1);
-  
-
   lcd.clear();
   lcd.setCursor(0,0);
   lcd.print("HC05-OK");
-  delay(1000);
+  delay(300);
   digitalWrite(LED_YELLOW, 0);
+  btm.println("AT+RESET"); // Send "AT" command
+  digitalWrite(BTM_KEY, 0);
+  delay(1000);
   digitalWrite(BUZZ, 0);
-
-
+  btm.begin(9600);
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("CAR CHECKING");
+  if (!connected) {
+  checkcar();
+  }
 } else {
   Serial.println("Connection failed");
 }
 }
 
-void loop() {
-  
+void loop()
+{
 
+  float tempLOAD = myELM327.engineLoad();
+  float tempRPM = myELM327.rpm();
+  // float tempTEMP = myELM327.engineCoolantTemp();
 
+  if (myELM327.nb_rx_state == ELM_SUCCESS)
+  {
+    rpm = (uint32_t)tempRPM;
+    load = (uint32_t)tempLOAD;
+    // temp = (uint32_t)tempTEMP;
 
+    if (load > 60)
+    {
+        digitalWrite(BUZZ, 1);
+    }
+    else{
+        digitalWrite(BUZZ, 0);
+    }
+    if (rpm > 2200)
+    {
+        digitalWrite(BUZZ, 1);
+    }
+    else{
+        digitalWrite(BUZZ, 0);
+    }
+    Serial.print("RPM: "); Serial.println(rpm);
+    Serial.print("TEMP: "); Serial.println(load);
+      lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print(rpm);
+  lcd.setCursor(0,1);
+  lcd.print(load);
+  }
+  else if (myELM327.nb_rx_state != ELM_GETTING_MSG)
+    myELM327.printError();
 
-  
 }
